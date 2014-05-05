3f75359c940ae85b1bf99955a5de946fd960bdfe
Theodoros Eleftherios Panou <cs071045@teiath.gr>
2014-05-05 21:58:43 +0300

    Add improved handling of incoming segments and feedback messages.

--- code/src/net.c
+++ code/src/net.c
@@ -10,40 +10,41 @@
 
 static uint8_t net_buffer[NET_BUF_SIZE];
 
-
 void socket0_handler(uint8_t status) {
 
-    printf("\nSOCKET STATUS: %x\n", status);
-
-    /* If a connection has been established, just remove the flag. */
-    if(status & Sn_IR_CON) {
-        puts("Socket 0: CON\n");
-    }
+    /* Print interrupt flags for this invocation. */
+    printf("\nSOCKET 0 Sn_IR: 0x%x (", status);
+    if(status & Sn_IR_SEND_OK) printf("[SEND_OK]");
+    if(status & Sn_IR_TIMEOUT) printf("[TIMEOUT]");
+    if(status & Sn_IR_RECV) printf("[RECV]");
+    if(status & Sn_IR_DISCON) printf("[DISCON]");
+    if(status & Sn_IR_CON) printf("[CON]");
+    printf(") Sn_SR: 0x%x\n", IINCHIP_READ(Sn_SR(0)));
 
     if(status & Sn_IR_RECV) {
         uint16_t rx_size = getSn_RX_RSR(0);
         uint16_t fragment;
 
-        uint16_t rx_RD;
-        printf("Socket 0: RECV. TOTAL SIZE: %d:\n", rx_size);
-        do {
+        printf("[RECV] size: %d\n", rx_size);
 
-            /* Read chunks from the incoming data, each up to a maximum of @c
-            * NET_BUF_SIZE bytes. Later, each chunk will be forwarded to the
-            * appropriate function. */
-            fragment = rx_size < NET_BUF_SIZE ? rx_size : NET_BUF_SIZE- 1;
-            recv(0, net_buffer, fragment);
-            net_buffer[fragment] = '\0';
+        if(rx_size > 0 ) {
 
-            printf("%s", net_buffer);
+            while(rx_size > 0) {
 
-            rx_size    -= fragment;
+                /* Read chunks from the incoming data, each up to a maximum of
+                * @c NET_BUF_SIZE bytes. Later, each chunk will be forwarded to
+                * the appropriate function. */
+                fragment = rx_size < NET_BUF_SIZE ? rx_size : NET_BUF_SIZE - 1;
+                recv(0, net_buffer, fragment);
+                net_buffer[fragment] = '\0';
 
-        } while(rx_size > 0);
+                /* Process each fragment (forward it to handler). */
 
-            rx_RD  = IINCHIP_READ(Sn_RX_RD0(0)) << 8;
-            rx_RD |= IINCHIP_READ(Sn_RX_RD0(0) + 1) & 0x00FF;
-        printf("\n>> RD: %x\n", rx_RD);
+                rx_size    -= fragment;
+            }
+
+            send(0, "HTTP/1.1 200\r\nContent-Type: text/html; charset: US-ASCII\r\nConnection: close\r\nContent-Length: 1\r\n\r\n@", 99);
+        }
     }
 
     /* Occurs when a connection termination is requested OR completed. */
@@ -52,7 +53,7 @@ void socket0_handler(uint8_t status) {
 
     }
     if(status & Sn_IR_TIMEOUT) {
-        puts("Socket 0: TIMEOUT\n");
+        puts("[TIMEOUT] Timeout has occurred.");
     }
 
     puts("=========================");
