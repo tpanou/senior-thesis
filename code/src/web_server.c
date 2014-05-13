7dc3476d00ce5fa940646e832b4629cfa8713873
Theodoros Eleftherios Panou <cs071045@teiath.gr>
2014-05-13 20:51:01 +0300

    Add functions to recognise CRLF and LWS on stream.

--- code/src/web_server.c
+++ code/src/web_server.c
@@ -1,3 +1,39 @@
 
 #include "webserver.h"
 #include "../build/util.h"
+
+int8_t is_LWS(uint8_t c) {
+
+    if(c == ' ' || c == '\t') return 1;
+
+    if(c == '\r') {
+        if(s_peek(&c, 0)) return 0;
+
+        if(c == '\n') {
+            if(s_peek(&c, 1)) return 0;
+
+            /* A CR followed by a LN and a LWSP-char is a valid LWS. */
+            if(c == ' ' || c == '\t') {
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+int8_t is_CRLF(uint8_t c) {
+
+    if(c == '\r') {
+        if(s_peek(&c, 0)) return 0;
+
+        if(c == '\n') {
+            if(s_peek(&c, 1)) return 0;
+
+            /* A CRLF is rendered a LWS, if it is followed by a LWS-char, ie,
+            * SPACE or HTAB. */
+            if(c != ' ' && c != '\t') return 1;
+        }
+    }
+
+    return 0;
+}
--- code/src/web_server.h
+++ code/src/web_server.h
@@ -4,4 +4,46 @@
 #ifndef WEB_SERVER_H_INCL
 #define WEB_SERVER_H_INCL
 
+#include <inttypes.h>
+
+/**
+* @brief Indicates a CRLF sequence.
+*/
+#define CRLF    -4
+
+/**
+* @brief Indicates any character (based on the context of its use).
+*/
+#define OTHER   -5
+
+#ifndef EOF
+#define EOF     -1
+#endif
+
+/**
+* @brief Checks whether there is a LWS on stream starting with @p c.
+*
+* A linear white space is an LWSP-char (ie, @c SPACE or @c HTAB) optionally
+* preceded by a CRLF sequence.
+*
+* This function does not consume bytes from the stream. It simple peeks forward.
+*
+* @param c The first character to use in the comparisons.
+* @returns @c 1, if true; @c 0, otherwise.
+*/
+int8_t is_LWS(uint8_t c);
+
+/**
+* @brief Checks whether there is a CRLF sequence on stream starting with @p c.
+*
+* It returns @c 0 (ie, false), if there is a CRLF sequence followed by an @c
+* LWSP-char which, semantically, is a header folding.
+*
+* This function does not consume bytes from the stream. It simply peeks forward.
+*
+* @param c The first character to use in the comparisons.
+* @returns @c 1, if true; @c 0, otherwise.
+*/
+int8_t is_CRLF(uint8_t c);
+
 #endif /* WEB_SERVER_H_INCL */
