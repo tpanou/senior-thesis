e1ff9492e668622c5c023dfc6c0c59c2c2193332
Theodoros Eleftherios Panou <cs071045@teiath.gr>
2014-07-13 21:35:07 +0300

    Add `ResourceHandler' struct to map an absolute path to its handler callback.

--- code/src/net.h
+++ code/src/net.h
@@ -9,3 +9,25 @@
 */
 void socket0_handler(uint8_t status);
 
+/**
+* @brief Callback function for a particular absolute path.
+*
+* Provides a callback for a particular (server defined) absolute path. The
+* callback function receives a pointer to the #HTTPRequest representation of the
+* request and may work with #s_next() and #c_next() to consume the input stream
+* and produce an appropriate response. It is safe to assume the callback is only
+* executed if the requested method (ie, #HTTPRequest::method) is applicable to
+* this particular path.
+*
+* All paths supported by the server must be issued exactly one handler. Use
+* #set_resource_handler() to that end. Failing to do so for some paths will
+* result in a 500 (Internal Server Error) being returned to the requester entity
+* for those paths.
+*/
+typedef struct {
+    /** @brief Value representing the URI of the request line. */
+    uint8_t path;
+
+    /** @brief Handler callback for the specified #ResourceHandler::path. */
+    void (*fn)(HTTPRequest *);
+} ResourceHandler;
