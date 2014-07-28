6002e60771647aac2d14e2f8e2b017ff131d4438
Theodoros Eleftherios Panou <cs071045@teiath.gr>
2014-07-28 23:58:42 +0300

    Add reading an array of strings from program into main memory.

--- code/src/util.c
+++ code/src/util.c
@@ -1,4 +1,7 @@
 #include "util.h"
+#include <avr/pgmspace.h>
+#include <stdarg.h>
+#include <string.h>
 
 uint8_t uint_to_str(uint8_t* buf, uint16_t number) {
     uint8_t unit;
@@ -15,4 +18,25 @@ uint8_t uint_to_str(uint8_t* buf, uint16_t number) {
         i           =  1;
     }
     return i;
+}
+
+void pgm_read_str_array(uint8_t** indices, uint8_t* buf, ...) {
+    va_list ap;         /* Pointer to each optional argument. */
+    PGM_P str;          /* Address of a string in Flash. */
+    uint8_t i = 0;      /* Index of @p indices to write to. */
+
+    va_start(ap, buf);
+    str = va_arg(ap, PGM_P);
+
+    /* Repeat for all supplied progmem strings. */
+    while(str != NULL) {
+        indices[i]  =  buf;             /* Store start address of the string. */
+
+        strcpy_P(buf, str);             /* Copy string into temporary buffer. */
+        buf        +=  strlen(buf) + 1; /* Prepare the next buffer address. +1
+        * is for null-byte. */
+
+        ++i;
+        str = va_arg(ap, PGM_P);
+    }
 }
--- code/src/util.h
+++ code/src/util.h
@@ -8,6 +8,13 @@
 
 #include <inttypes.h>
 
+#ifndef NULL
+/**
+* Specify that a pointer has not been set to a valid address.
+*/
+#define NULL 0
+#endif
+
 /**
 * @brief Convert an unsigned integer to string.
 *
@@ -22,5 +29,26 @@
 * @returns The amount of digits written (null-byte not included).
 */
 uint8_t int_to_str(uint8_t* buf, uint16_t number);
+
+/**
+* @brief Read a number of program memory chunks into @p buf.
+*
+* Accepts a variable amount of addresses of strings that reside in program
+* memory. The strings are loaded into @p buf and the address of the first byte
+* of each is placed into @p indices.
+*
+* Some notes:
+*   - The last argument *should* always be @c NULL.
+*   - @p indices should be as large as the number of string addresses given
+*       (excluding @c NULL).
+*   - @p buf should be large enough to accommodate all requested strings.
+*
+* @param[out] indices Array of strings read from program memory.
+* @param[out] buf Buffer into which to store the strings read from program
+*   memory.
+* @param[in] ... Program memory addresses to read strings from. The last one
+*   *should* be @c NULL.
+*/
+void pgm_read_str_array(uint8_t** indices, uint8_t* buf, ...);
 
 #endif /* UTIL_H_INCL */
