ac869599a15cb9ad36b2fbe125fdaf30be7a4c2c
Theodoros Eleftherios Panou <cs071045@teiath.gr>
2014-08-19 17:41:07 +0300

    Add javascript menu wrapper to help shift between menus.

--- code/src/ui/client.js
+++ code/src/ui/client.js
@@ -12,6 +12,95 @@
         "#content"      : handle_show_log
     }
 
+    /**
+    * @brief A simplistic menu bar wrapper.
+    *
+    * @param[in] elNav An element that contains the menus.
+    * @param[in] selMenu A selector that returns the elements each one of which
+    * constitutes a menu within `elNav'. These elements are applied class
+    *   `clsActive' to change their appearance as active.
+    * @param[in] clsActive The class (with no dot prefixed) that defines a menu
+    *   element as active/selected.
+    */
+    var MAIN_MENU = (function MenuBar(elNav, selMenu, clsActive) {
+        /* Pairs of a hash and the element that contains an anchor to that hash
+        * (see init()). */
+        var menuItems;
+
+        /**
+        * @brief Initialise references to a menu bar.
+        *
+        * Populates an internal structure with references to menu elements (as
+        * selected with `selMenu'). Each such element is correlated to a URL
+        * fragment which is also required to activate a menu over another (see
+        * `selectMenu()').
+        *
+        * Note that the URL fragment is extracted from the `href' attribute of
+        * the *first* anchor element descendant of each menu. Also note that the
+        * URL fragments must be unique for each menu within a particular
+        * `elNav'.
+        */
+        var init = function() {
+            var menus,              // All menus in elNav
+                menu,               // A single menu from `menus'
+                i,                  // Iterator over `menus'
+                hash;               // The `href' of the anchor within `menu'
+
+            menuItems   =  {};      // Remove any previously set menus.
+            menus       =  elNav.querySelectorAll(selMenu);
+
+            /* For all menu elements within elNav, fetch their first descendant
+            * anchor in order to store the URL fragment of that menu in
+            * `menuItems'. */
+            for(i = 0 ; i < menus.length ; ++i) {
+                menu            =  menus[i];
+                hash            =  menu.querySelector("a").getAttribute("href");
+                menuItems[hash] =  menu;
+            }
+        };
+
+        /**
+        * @brief Apply `clsActive' to the menu that corresponds to `hash'.
+        *
+        * 
+        * Any previously selected menu is always deselected, even if `hash' does
+        * not identify a particular menu.
+        *
+        * @param[in] hash The menu to activate/select.
+        * @returns @c 1, if a new menu was selected; @c 0 otherwise.
+        */
+        var selectMenu = function(hash) {
+            var curMenu;        // Currently selected menu.
+
+            /* Deselect the current menu, even if the new one is not valid. */
+            curMenu =  elNav.querySelector("." + clsActive);
+            if(curMenu) {
+                /* Remove class `clsActive'. Spaces around the class name are
+                * added to avoid replacing a partial match.  */
+                curMenu.className = (" " + curMenu.className + " ")
+                                    .replace(" " + clsActive + " ", "")
+                                    .replace(/^\s+|\s+$/g, "");
+            }
+
+            /* Select the new menu, if one with such a hash exists. */
+            if(menuItems[hash]) {
+                menuItems[hash].className += " " + clsActive;
+                return 1;
+            }
+            return 0;
+        };
+
+        /* Initialise the menu bar. */
+        init(elNav, selMenu, clsActive);
+
+        /* Return access to the public functions of this object. */
+        return { "init"         : init,
+                 "selectMenu"   : selectMenu
+        };
+
+    })(document.getElementById("nav"), "li", "current");
+
+
     /* Add listeners to update the page content when the URL fragment is
     * altered. Reloading the state `onhashchange' responds to hash changes due
     * to history traversal (eg, pressing back or forward), whereas `onload'
