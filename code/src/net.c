da632eecaf9dc74ebc385727184ba5555a0c21a2
Theodoros Eleftherios Panou <cs071045@teiath.gr>
2014-08-31 13:52:26 +0300

    Add W5100 ISR on pin INT1.

--- code/src/net.c
+++ code/src/net.c
@@ -1,15 +1,39 @@
 #include "net.h"
 #include "defs.h"
-#include "w5100/w5100.h"
+#include "w5100.h"
 #include "http_server.h"
 
 #include <avr/io.h>
-#include <stdio.h>
-#include <inttypes.h>
+#include <avr/interrupt.h>
 
 
+/**
+* @brief Propagates interrupts from the W5100 to the appropriate handlers.
+*
+* This ISR reads the #NET_IR to determine the socket source and calls the
+* appropriate handler. Currently, only one socket operates, which is responsible
+* for the HTTP server transactions (see handle_http_socket()). Upon invoking the
+* handler, the ISR keeps a copy of the socket's interrupt register (#NET_Sn_IR)
+* and passes its value as an argument. The handler is supposed to respond to the
+* specified flags only. Once completed, the ISR will clear the interrupt flag
+* bits that were specified to the handler. If more interrupt flag bits have been
+* set in the meantime, the ISR will be ready to respond to them.
+*/
+ISR(INT1_vect) {
+    uint8_t status = net_read8(NET_IR);
+    uint8_t socket;
 
+    /* Only HTTP_SOCKET interrupts are of interest; clear high nibble. */
+    net_write8(NET_IR, 0xE0);
 
+    if(bit_is_set(status, HTTP_SOCKET)) {
+        socket  =  net_read8(NET_Sn_IR(HTTP_SOCKET));
+        handle_http_socket(HTTP_SOCKET, socket);
+
+        /* Clear Socket interrupt flags that have just been dealt with. */
+        net_write8(NET_Sn_IR(HTTP_SOCKET), socket);
+    }
+}
 
 void socket0_handler(uint8_t status) {
 
