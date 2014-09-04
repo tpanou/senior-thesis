e7ebcaf0bbeba7e86707d440d500f187f1e9ed55
Theodoros Eleftherios Panou <cs071045@teiath.gr>
2014-09-04 05:29:57 +0300

    Fix older macro comment layout.

--- code/src/defs.h
+++ code/src/defs.h
@@ -204,28 +204,27 @@ void sys_get(uint8_t setting, void* value);
 int8_t sys_set(uint8_t setting, void* value);
 
 /**
-@brief The frequency of CPU clock, required by <avr/delay.h> convenience
-functions.
+* @brief Frequency of CPU clock, required by <avr/delay.h>.
 */
 #define F_CPU (4000000UL)
 
 /**
-@brief USART baud rate.
-
-For the current CPU clock frequency (4MHz), in Asynchronous normal mode, a
-minimal error of 0.2\% is achieved at a baud rate of 19.2kbps. *Atmel
-pp.190-193.*
+* @brief USART baud rate.
+*
+* For the current CPU clock frequency (4MHz), in Asynchronous normal mode, a
+* minimal error of 0.2\% is achieved at a baud rate of 19.2kbps. *Atmel
+* pp.190-193*
 */
 #define USART_BAUD (19200)
 
 /**
-@brief Calculates the value for the baud rate register.
-
-For Asynchronous normal mode, the value of UBRR is calculated from the following
-formula: \f[ UBRR = \frac{f_{OSC}}{16 BAUD} - 1 \f] where\n
-\f$f_{OSC}\f$ is the System Oscillater clock frequency which, in this case is
-down-scaled to 4MHz, and\n
-\f$BAUD\f$ is the baud rate (in bps).
+* @brief Calculates the value for the baud rate register.
+*
+* For Asynchronous normal mode, the value of UBRR is calculated from the
+* following formula: \f[ UBRR = \frac{f_{OSC}}{16 BAUD} - 1 \f] where\n
+* \f$f_{OSC}\f$ is the System Oscillator clock frequency which, in this case is
+* down-scaled to 4MHz, and\n
+* \f$BAUD\f$ is the baud rate (in bps).
 */
 #define UBRR_VALUE (int)(F_CPU/16/USART_BAUD - 1)
 
